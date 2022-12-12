b38b996a528be713ca0c38b3c4c786545fdc575a
Theel <111762535+Theel@users.noreply.github.com>
2022-12-12 16:52:59 +0100

    Add files via upload

--- hh_gas.py
+++ hh_gas.py
@@ -2,11 +2,13 @@ import os
 from pandapipes.io.file_io import from_json
 import pandapipes.plotting as plot
 import pandapipes_to_dymola as p_to_d
-filename = "hh_gas.json"
+filename = "hh_gas"
 
-net = from_json(os.path.join("network_files", filename))
+net = from_json(os.path.join("network_files", filename+".json"))
 plot.simple_plot(net, pipe_width=2.0, junction_size=1.0, plot_sinks=True, plot_sources=True, sink_size=1.0, source_size=1.0, show_plot=True)
 
 p_to_d.CDB_to_Modelica(net, modelName="hh_gas", xy_scale=5000)
+
+
 
 
---
+++ writeController.py
@@ -0,0 +1,44 @@
+
+import os
+import csv
+from scipy.io import matlab
+from pandapipes.io.file_io import from_json
+import pandas as pd
+from pandapipes.control.run_control import run_control
+import pandapower.control as control
+import pandapipes.networks as networks
+
+from pandapower.timeseries import DFData
+from pandapower.timeseries import OutputWriter
+from pandapipes.timeseries import run_timeseries
+
+def controller(net, filename, directory="files" ,model_type="sink_at_"):
+    df = pd.read_csv(directory+ "/" +filename+".csv")
+    i=0
+    for (columnName, columnData) in df.items():
+        if columnName != 'Datetime':
+            Data=pd.DataFrame(columnData)
+            corretDataForm=DFData(Data)
+            globals()[f"contoler{columnName}"]=control.ConstControl(net, element="sink", variable='mdot_kg_per_s',
+                                                    element_index=net.sink.index.values[i], data_source=corretDataForm,
+                                                    profile_name=net.sink.index.values[i].astype(str))
+            i+=1
+
+    #print(globals()[f"contoler{columnName}"])
+
+    time_steps = range(columnData.size)
+    print(f'timesteps={time_steps}')
+    log_variables = [('res_junction', 'p_bar'),
+                     ('res_pipe', 'v_mean_m_per_s'), ('res_pipe', 'reynolds'), ('res_pipe', 'lambda'),
+                     ('res_sink', 'mdot_kg_per_s'),
+                     ('res_ext_grid', 'mdot_kg_per_s')]
+    ow = OutputWriter(net, time_steps, output_path='results', output_file_type='.csv', log_variables=log_variables)
+    #
+    #
+
+    run_timeseries(net, time_steps)
+    #run_control(net)
+filename = "example"
+net = from_json(os.path.join("network_files", filename+".json"))
+controller(net, "example_loadprofiles_kg_per_s")
+print('hello')
